d3d05d54a37d6eb0dcd84107d93e5d11501107cd
Jisha-Maniamma <jishapillai1991@yahoo.co.in>
2021-06-27 19:10:40 +0900

    1

--- src/org/jishaStudy/Interfaces_example/test.java
+++ src/org/jishaStudy/Interfaces_example/test.java
@@ -1,16 +1,27 @@
 package org.jishaStudy.Interfaces_example;
 
-public class test implements  it22 {
-    @Override
-    public void m1() {
-        System.out.println("aa");
-    }
+public class test extends  it33 {
+  //  @Override
+
 
     public static void main(String[] args) {
         test t=new test();
         t.m1();
+        t.m2();
+    }
+
+    @Override
+    public void m2() {
+
     }
 }
 interface it22{
     void m1();
+    void m2();
 }
+abstract class it33 implements it22 {
+    public void m1() {
+        System.out.println("aa");
+    }
+}
+
